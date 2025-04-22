6b8df24a65e92de1b091adfa190761f80b3cef9d
jwkidd3 <jkidd@kiddcorp.com>
2025-04-22 09:14:26 -0500

    more

---
+++ Module 6/Container/SendMessages/SendMessages/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMessages
+{
+    public class Order
+    {
+        public int orderId { get; set; }
+        public string productName { get; set; }
+        public int quantity { get; set; }
+
+    }
+
+
+}
---
+++ Module 6/Container/SendMessages/SendMessages/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Amazon.SQS.Model;
+using Amazon.SQS;
+using SendMessages;
+using Newtonsoft.Json;
+
+string queueURL = "https://sqs.ap-south-1.amazonaws.com/213171387512/appqueue";
+var sqsClient = new AmazonSQSClient();
+
+for (int i=1; i<=5;i++)
+{
+    Order order=new Order();
+    order.orderId = i;
+    order.productName = $"Product{i}";
+    order.quantity = (i * 5);
+    await SendMessage(sqsClient,queueURL, JsonConvert.SerializeObject(order));
+}
+
+static async Task SendMessage(
+      IAmazonSQS sqsClient, string queueUrl, string messageBody)
+{
+    SendMessageResponse msgResponse =
+      await sqsClient.SendMessageAsync(queueUrl, messageBody);
+      Console.WriteLine("Message added to queue");
+}
+
